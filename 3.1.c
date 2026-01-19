1dc9b322eb74b94e11f4aed3c2d7844d5f94e84b
Yaroslaw Ruazanov TKI-141 <panda2490@bk.ru>
2026-01-19 18:32:43 +0300

    Add files via upload

--- 3.1.c
+++ 3.1.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+﻿﻿#include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
 #include <stdbool.h>
@@ -18,36 +18,50 @@ double getValue();
 */
 double func(const double x);
 
+/**
+ * @brief проверят, больше ли старт функции или нет
+ * @return возвращает ошибку, если старт больше чем конец, иначе ничего не возвращает
+ */
+void checkStartEnd(const double start, const double end);
 
 /**
 * @brief проверяет, что переменная положительна
 * @param step - значение проверяемой переменной
 */
-void checkValue(const double step);
+void checkStep(const double step);
 
+/**
+* @brief Точка входа в программу
+* @return возвращает 0, если прогамма выполнена корректно
+*/
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+
 	printf("Введите начальное значение интервала: ");
-	double start = getValue();
+	const double start = getValue();
 	printf("Введите конечное значение интервала: ");
-	double end = getValue();
+	const double end = getValue();
+	checkStartEnd(start, end);
+
 	printf("Введите шаг: ");
-	double step = getValue();
-	checkValue(step);
-	for(double x = start; x < end+step+DBL_EPSILON;x=x+step)
-	{ 
+	const double step = getValue();
+	checkStep(step);
+
+	for (double x = start; x <= end + DBL_EPSILON;x = x + step)
+	{
 		printf("x = %.2lf, y = %.4lf\n", x, func(x));
 	}
 	return 0;
 }
-double getValue()
+
+double getValue(void)
 {
 	double value = 0;
 	if (!scanf_s("%lf", &value))
 	{
-		printf("Error\n");
-		abort();
+		fprintf(stderr, "Error, не корректный ввод значения переменной value\n");
+		exit(1);
 	}
 	return value;
 
@@ -58,11 +72,20 @@ double func(const double x)
 	return 3 * x - 14 + exp(x) - exp(-x);
 }
 
-void checkValue(const double step)
+void checkStep(const double step)
 {
 	if (step <= DBL_EPSILON)
 	{
-		printf("Value have to be positive\n");
-		abort();
+		fprintf(stderr, "Error, шаг должен быть положительным\n");
+		exit(1);
+	}
+}
+
+void checkStartEnd(const double start, const double end)
+{
+	if (start > end + DBL_EPSILON)
+	{
+		fprintf(stderr, "Error, начальное значение интервала больше конечного");
+		exit(1);
 	}
 }
