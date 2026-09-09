5ac2cb4f5455316ca69b73422559fb061fbea28f
Ярослав Рязанов TKI-241 <panda2490@bk.ru>
2026-09-09 12:01:07 +0300

    Update AcademicDegree.h

--- AcademicDegree.h
+++ AcademicDegree.h
@@ -12,34 +12,40 @@ private:
 public:
 
     /**
-       @brief Конструктор по умолчанию.
+       @brief Конструктор по умолчанию
     */
     AcademicDegree();
 
     /**
-       @brief Конструктор с параметром.
-       @param degree Строковое представление учёной степени.
+       @brief Конструктор с параметром
+       @param degree Строковое представление учёной степени
     */
     AcademicDegree(const std::string& degree);
 
     /**
-       @brief Преобразует учёную степень в строковое представление.
+       @brief Преобразует учёную степень в строковое представление
     */
     std::string toString() const;
 
+    /**
+       @brief Оператор ввода из потока 
+       @param is Поток ввода
+       @param rank Объект научного звания для заполнения
+       @return Ссылка на поток ввода для цепочки операций
+    */
     friend std::istream& operator>>(std::istream& is, AcademicDegree& degree);
 
     /**
-       @brief Оператор сравнения на равенство.
-       @param other Другая учёная степень для сравнения.
-       @return true, если степени равны, иначе false.
+       @brief Оператор сравнения на равенство
+       @param other Другая учёная степень для сравнения
+       @return true, если степени равны, иначе false
     */
     bool operator==(const AcademicDegree& other) const;
 
     /**
-       @brief Оператор сравнения на не равенство.
-       @param other Другая учёная степень для сравнения.
-       @return true, если степени не равны, иначе false.
+       @brief Оператор сравнения на не равенство
+       @param other Другая учёная степень для сравнения
+       @return true, если степени не равны, иначе false
     */
     bool operator!=(const AcademicDegree& other) const;
 };
