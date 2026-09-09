b3e1fb7545c3dadc3a849b9504451c674e84f865
Ярослав Рязанов TKI-241 <panda2490@bk.ru>
2026-09-09 11:39:35 +0300

    Update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -1,8 +1,23 @@
 #include "triangle.h"
 #include <cmath>
+#include <limits>
+
+static bool isZero(double value) {
+    return std::abs(value) < std::numeric_limits<double>::epsilon() * 1000;
+}
+
+void triangle::validateTriangle(const point& A, const point& B, const point& C) const {
+    double crossLengthSquared = crossX * crossX + crossY * crossY + crossZ * crossZ;
+    double area = std::sqrt(crossLengthSquared) / 2.0;
+    if (isZero(area)) {
+        std::cout << "Такого треугольника не существует" << std::endl;
+        exit(1);
+    }
+}
 
 triangle::triangle(const point& A, const point& B, const point& C)
     : A(A), B(B), C(C) {
+    validateTriangle(A, B, C);
 }
 
 double triangle::sideLength(const point& P, const point& Q) const {
