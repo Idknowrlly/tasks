f255c51040ebcf74647c2ab7ef9c601c7dc9db3c
Ярослав Рязанов TKI-241 <panda2490@bk.ru>
2026-09-09 11:52:05 +0300

    Update AcademicDegree.h

--- AcademicDegree.h
+++ AcademicDegree.h
@@ -10,10 +10,36 @@ private:
     std::string value;  ///< текущая степень
 
 public:
+
+    /**
+       @brief Конструктор по умолчанию.
+    */
     AcademicDegree();
+
+    /**
+       @brief Конструктор с параметром.
+       @param degree Строковое представление учёной степени.
+    */
     AcademicDegree(const std::string& degree);
+
+    /**
+       @brief Преобразует учёную степень в строковое представление.
+    */
     std::string toString() const;
+
     friend std::istream& operator>>(std::istream& is, AcademicDegree& degree);
+
+    /**
+       @brief Оператор сравнения на равенство.
+       @param other Другая учёная степень для сравнения.
+       @return true, если степени равны, иначе false.
+    */
     bool operator==(const AcademicDegree& other) const;
+
+    /**
+       @brief Оператор сравнения на не равенство.
+       @param other Другая учёная степень для сравнения.
+       @return true, если степени не равны, иначе false.
+    */
     bool operator!=(const AcademicDegree& other) const;
 };
