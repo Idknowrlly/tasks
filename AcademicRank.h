b540262a4527a044054a82fa12fae913c830d12d
Ярослав Рязанов TKI-241 <panda2490@bk.ru>
2026-09-09 11:59:12 +0300

    Update AcademicRank.h

--- AcademicRank.h
+++ AcademicRank.h
@@ -10,10 +10,42 @@ private:
     std::string value;  ///< текущее звание
 
 public:
+
+    /**
+       @brief Конструктор по умолчанию
+    */ 
     AcademicRank();
+
+    /**
+       @brief Конструктор с параметром
+       @param rank Строковое представление научного звания
+    */
     AcademicRank(const std::string& rank);
+
+    /**
+       @brief Преобразует научное звание в строковое представление
+    */
     std::string toString() const;
+
+    /**
+       @brief Оператор ввода из потока
+       @param is Поток ввода
+       @param rank Объект научного звания для заполнения
+       @return Ссылка на поток ввода для цепочки операций
+    */
     friend std::istream& operator>>(std::istream& is, AcademicRank& rank);
+
+    /**
+       @brief Оператор сравнения на равенство
+       @param other Другое научное звание для сравнения
+       @return true, если звания равны, иначе false
+    */
     bool operator==(const AcademicRank& other) const;
+
+    /**
+       @brief Оператор сравнения на не равенство
+       @param other Другое научное звание для сравнения
+       @return true, если звания не равны, иначе false
+    */
     bool operator!=(const AcademicRank& other) const;
 };
