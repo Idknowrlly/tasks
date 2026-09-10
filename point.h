7ad36b7b52a755eba73de95a1bf44146464a4d18
Ярослав Рязанов TKI-241 <panda2490@bk.ru>
2026-09-10 17:29:43 +0300

    Update point.h

--- point.h
+++ point.h
@@ -1,20 +1,20 @@
 #pragma once
 /**
     @brief   класс точка
-**/
+*/
 class point {
 private:
     /**
         @brief переменная x
-    **/
+    */
     double x1;
     /**
         @brief переменная y
-    **/
+    */
     double y1;
     /**
         @brief переменная z
-    **/
+    */
     double z1;
 public:
     /**
@@ -22,21 +22,21 @@ public:
         @param x - координата точки
         @param y - координата точки
         @param z - координата точки
-    **/
+    */
     point(const double x1 = 0, const double y1 = 0, const double z1 = 0);
     /**
      * @breif получить координату точки
      * @return - координата x
-     */
+    */
     double x() const;
     /**
      * @breif получить координату точки
      * @return - координата y
-     */
+    */
     double y() const;
     /**
      * @breif получить координату точки
      * @return - координата z
-     */
+    */
     double z() const;
 };
